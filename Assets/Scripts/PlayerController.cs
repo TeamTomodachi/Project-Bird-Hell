48394bb33443b054c158863a4efb719c70aca706
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-10-18 17:55:40 -0400

    Stamina Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,47 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerStamina))]
 public class PlayerController : MonoBehaviour {
 	public const float PLAYER_JUMP_SPEED = 6.0f;
 	public const float PLAYER_MOVEMENT_SPEED = 8.0f;
+	public const float STAMINA_JUMP_COST = 10.0f;
 
 	[SerializeField]
 	public PlayerInfo Player = new PlayerInfo();
 
-	//CharacterController m_characterController;
+	// Unity Components
 	Rigidbody2D m_rigidbody2D;
-	SpriteRenderer m_spriteRenderer;
+	// Custom Components
+	PlayerStamina m_stamina;
+
+	public bool CanJump { get { return (m_stamina.CurrentStamina - STAMINA_JUMP_COST) >= 0.0f; } }
 
 	// Use this for initialization
 	void Start () {
-		//m_characterController = GetComponent<CharacterController>();
 		m_rigidbody2D = GetComponent<Rigidbody2D>();
-		m_spriteRenderer = GetComponent<SpriteRenderer>();
+		m_stamina = GetComponent<PlayerStamina>();
 	}
 	
 	// Update is called once per frame
-	void Update () {        
+	void Update () {
+		// Preform Jump
+		if (Input.GetButtonDown(Player.ControllerPrefix + "Jump") &&
+			CanJump)
+		{
+			//Debug.Log("Jump");
+			m_stamina.DecreaseStamina(STAMINA_JUMP_COST);
+
+			var vel = m_rigidbody2D.velocity;
+			vel.y = PLAYER_JUMP_SPEED;
+			m_rigidbody2D.velocity = vel;
+		}
 	}
 
 	// Called once per Physics Update
 	private void FixedUpdate()
 	{
 		float horizontalMovement = Input.GetAxis(Player.ControllerPrefix + "Horizontal");
-        m_rigidbody2D.transform.position += (new Vector3(horizontalMovement, 0.0f) * Time.fixedDeltaTime) * PLAYER_MOVEMENT_SPEED;
+		m_rigidbody2D.transform.position += (new Vector3(horizontalMovement, 0.0f) * Time.fixedDeltaTime) * PLAYER_MOVEMENT_SPEED;
 
-        // Flip the sprite to face the direction of movement 
-        var scale = transform.localScale;
-        if (horizontalMovement > 0.001f) { scale.x = 1; } // Flip Right
+		// Flip the sprite to face the direction of movement 
+		var scale = transform.localScale;
+		if (horizontalMovement > 0.001f) { scale.x = 1; } // Flip Right
 		else if (horizontalMovement <= -0.001f) { scale.x = -1; } // Flip Left
-        transform.localScale = scale;
-
-        // Preform Jump
-		if (Input.GetButtonDown(Player.ControllerPrefix+"Jump"))
-        {
-			//Debug.Log("Jump");
-			var vel = m_rigidbody2D.velocity;
-			vel.y = PLAYER_JUMP_SPEED;
-			m_rigidbody2D.velocity = vel;
-		}
+		transform.localScale = scale;
 	}
 }
---
+++ Assets/Scripts/PlayerStamina.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStamina : MonoBehaviour {
+    public const float MIN_STAMINA = 0.0f;
+    public const float MAX_STAMINA = 100.0f;
+    public const float STAMINA_INCREASE_RATE = 10.0f;
+    public float CurrentStamina = MAX_STAMINA;
+
+    public void IncreaseStamina(float amount)
+    {
+        CurrentStamina = Mathf.Min(CurrentStamina + amount, MAX_STAMINA);
+    }
+    public void DecreaseStamina(float amount)
+    {
+        CurrentStamina = Mathf.Max(CurrentStamina - amount, MIN_STAMINA);
+    }
+
+    bool OnFloor = false;
+
+    private void Update()
+    {
+        if (OnFloor)
+        {
+            IncreaseStamina(STAMINA_INCREASE_RATE * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            OnFloor = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            OnFloor = false;
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+    }
+}
