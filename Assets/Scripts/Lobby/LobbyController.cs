fe866bbffdbf15c0a47cdd8d32db8501bf58cab2
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-10-21 17:14:28 -0400

    Begun development of menus
    #3

---
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyController : MonoBehaviour {
+
+	public PlayerPanelController Player1Panel;
+	public PlayerPanelController Player2Panel;
+	public PlayerPanelController Player3Panel;
+	public PlayerPanelController Player4Panel;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Lobby/PlayerPanelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPanelController : MonoBehaviour {
+
+	public PlayerInfo PlayerInfo = new PlayerInfo();
+	public Color CursorColor;
+	private CursorController Cursor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
