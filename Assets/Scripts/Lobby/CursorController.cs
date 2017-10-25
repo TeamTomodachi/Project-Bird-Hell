463c031436038560960c78270cf16ee573ccb0f2
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-10-25 14:13:25 -0400

    Bound Cursor to Canvas

--- Assets/Scripts/Lobby/CursorController.cs
+++ Assets/Scripts/Lobby/CursorController.cs
@@ -8,16 +8,35 @@ public class CursorController : MonoBehaviour {
 
 	public PlayerPanelController PlayerPanel;
 	public Image CursorImage;
-
+    public RectTransform parentTransform;
 
 	// Use this for initialization
 	void Start () {
+        parentTransform = transform.parent as RectTransform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float horizontalMovement = Input.GetAxis(PlayerPanel.PlayerInfo.JoystickInputManagerPrefix + "Horizontal");
 		float verticalMovement = Input.GetAxis(PlayerPanel.PlayerInfo.JoystickInputManagerPrefix + "Vertical");
-		transform.position += (new Vector3(horizontalMovement, verticalMovement) * Time.fixedDeltaTime) * CURSOR_MOVEMENT_SPEED;
+		var newPosition = transform.position + (new Vector3(horizontalMovement, verticalMovement) * Time.fixedDeltaTime) * CURSOR_MOVEMENT_SPEED;
+
+        //Debug.Log(parentTransform.rect.ToString());
+
+        var properXMin = parentTransform.rect.xMin + (parentTransform.rect.width / 2);
+        var properXMax = parentTransform.rect.xMax + (parentTransform.rect.width / 2);
+        if (newPosition.x > properXMax)
+            newPosition.x = properXMax;
+        else if (newPosition.x < properXMin)
+            newPosition.x = properXMin;
+
+        var properYMin = parentTransform.rect.yMin + (parentTransform.rect.height / 2);
+        var properYMax = parentTransform.rect.yMax + (parentTransform.rect.height / 2);
+        if (newPosition.y > properYMax)
+            newPosition.y = properYMax;
+        else if (newPosition.y < properYMin)
+            newPosition.y = properYMin;
+
+        transform.position = newPosition;
 	}
 }
