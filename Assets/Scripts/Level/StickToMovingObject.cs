b65875646f5fce691643387454e0b409e9c9f977
Patrick Gingras <ging0044@algonquinlive.com>
2017-11-17 13:51:08 -0500

    Added script to move player with platform on collision, as per #25. Doesn't work so well if platform moves up and down.

---
+++ Assets/Scripts/Level/StickToMovingObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickToMovingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PreviousPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OnPlatform) {
+			var deltaPosition = transform.position - PreviousPosition;
+			deltaPosition.z = 0f;
+			player.transform.position += deltaPosition;
+		}
+		PreviousPosition = gameObject.transform.position;
+	}
+
+	private Vector3 PreviousPosition;
+
+	private bool OnPlatform;
+
+	private GameObject player;
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			OnPlatform = true;
+			player = collision.gameObject;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Player")
+			OnPlatform = false;
+	}
+}
