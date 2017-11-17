2ca2c93186da0cbe46de79cabb18036086cbdd47
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-11-17 14:17:59 -0500

    Fixed the Y movement
    #25

--- Assets/Scripts/Level/StickToMovingObject.cs
+++ Assets/Scripts/Level/StickToMovingObject.cs
@@ -2,38 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StickToMovingObject : MonoBehaviour {
+public class StickToMovingObject : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		PreviousPosition = gameObject.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (OnPlatform) {
-			var deltaPosition = transform.position - PreviousPosition;
-			deltaPosition.z = 0f;
-			player.transform.position += deltaPosition;
-		}
-		PreviousPosition = gameObject.transform.position;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        PreviousPosition = gameObject.transform.position;
+    }
 
-	private Vector3 PreviousPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        var deltaPosition = transform.position - PreviousPosition;
+        deltaPosition.z = 0f;
 
-	private bool OnPlatform;
+        foreach (var player in PlayersOnPlatform)
+        {
+            player.transform.position += deltaPosition;
+        }
 
-	private GameObject player;
+        PreviousPosition = gameObject.transform.position;
+    }
 
-	private void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.gameObject.tag == "Player") {
-			OnPlatform = true;
-			player = collision.gameObject;
-		}
-	}
+    private Vector3 PreviousPosition;
+    public List<GameObject> PlayersOnPlatform = new List<GameObject>();
+    public void BeginTracking(GameObject gObj)
+    {
+        if (gObj.tag == "Player")
+        {
+            PlayersOnPlatform.Add(gObj);
+        }
+    }
+    public void EndTracking(GameObject gObj)
+    {
+        if (gObj.tag == "Player")
+        {
+            PlayersOnPlatform.Remove(gObj);
+        }
+    }
 
-	private void OnCollisionExit2D(Collision2D collision) {
-		if (collision.gameObject.tag == "Player")
-			OnPlatform = false;
-	}
+    // Collision Events
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BeginTracking(collision.gameObject);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        EndTracking(collision.gameObject);
+    }
+
+    // Trigger Events
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BeginTracking(collision.gameObject);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        EndTracking(collision.gameObject);
+    }
 }
